4572e44e940e4447f052d7bd95dc17423f012c70
faramozzayw <faramo.zayw@gmail.com>
2020-04-25 13:27:55 +0000

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,76 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+	Hero,
+	HeroHeader,
+	HeroBody,
+	HeroFooter,
+	Container,
+	Field,
+	Label,
+	Control,
+	Input,
+	Title
+} from "bloomer";
+
+const App = () => {
+	return (
+		<div className="App">
+			<main>
+				<Hero isColor="success" isFullHeight>
+					<HeroHeader>
+						<Title
+							isSize={4}
+							tag="h4"
+							style={{
+								"padding": "2.5vmin"
+							}}
+						>Кредитний калькулятор</Title>
+					</HeroHeader>
+					<HeroBody>
+						<Container hasTextAlign='centered'>
+							<form >
+								<Field>
+									<Label hasTextColor="light">Сума виплат</Label>
+									<Control>
+										<Input
+											isSize="medium"
+											type="text"
+											placeholder='Введіть суму виплат'
+										/>
+									</Control>
+								</Field>
+								<Field>
+									<Label hasTextColor="light">Відсоткова ставка</Label>
+									<Control>
+										<Input 
+											isSize="medium"
+											type="text"
+											placeholder='Введіть відсоткову ставку'
+										/>
+									</Control>
+								</Field>
+								<Field>
+									<Label hasTextColor="light">Термін кредитування</Label>
+									<Control>
+										<Input 
+											isSize="medium"
+											type="text"
+											placeholder='Введіть термін кредитування'
+										/>
+									</Control>
+								</Field>
+							</form>
+						</Container>
+					</HeroBody>
+					<HeroFooter>
+						Powered by faramo_zayw
+					</HeroFooter>
+				</Hero>
+			</main>
+		</div>
+	);
 }
 
 export default App;
